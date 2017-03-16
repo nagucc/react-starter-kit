0c7d6282dd486299d0d79d37f806d366b2432206
na57 <na57@qq.com>
2017-03-16 12:52:32 +0800

    add: redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 import user from './user';
 import runtime from './runtime';
 import wechat from './wechat';
 
 export default combineReducers({
+  form,
   wechat,
   user,
   runtime,
